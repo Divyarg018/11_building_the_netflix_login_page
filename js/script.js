c973ad42a1c1ed3f99dc9aa7b1120bc11a8b92b7
Divya R <kwrdivyaraj100@gmail.com>
2023-05-13 23:05:40 +0530

    finishing the form validation

--- js/script.js
+++ js/script.js
@@ -19,12 +19,32 @@ function validateInput(e) {
       } else {
            classes = states[0];
       }
+
+      console.log(classes);
+
+      e.target.nextElementSibling.classList.remove(...states);
+
+     
+     e.target.nextElementSibling.classList.add(classes);
+
+
       if(classes === 'not-valid') {
+          if(e.target.parentElement.nextElementSibling.classList[0] !== 'alert' ) {
         const errorDiv = document.createElement('div');
         errorDiv.appendChild(document.createTextNode('This field is mandatory'));
-        
+        errorDiv.classList.add('alert');
+
         console.log(errorDiv);
-     
+
         e.target.parentElement.parentElement.insertBefore(errorDiv, e.target.parentElement.nextElementSibling);
-   }
+     }
+} else {
+   
+     if(e.target.parentElement.nextElementSibling.classList[0] === 'alert' ) {
+        
+          e.target.parentElement.nextElementSibling.remove();
+     }
 }
+
+}
+     
